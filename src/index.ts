b8b0267f779c296451a0c3b2d1baf75c2a10a7e9
Teddy Burns <code@teddyburns.com>
2024-12-07 10:33:37 -0500

    Add dynamic runner

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,31 @@
+import { jack } from "jackspeak";
+
 import Day1 from "./day01/day01";
 
-Day1();
+type AoCFunctionMap = {
+  [year: number]: {
+    [day: number]: () => void;
+  };
+};
+
+const years: AoCFunctionMap = {
+  2024: {
+    1: Day1,
+  },
+};
+
+const { values } = jack({})
+  .opt({
+    year: {
+      description: "Year of the Advent Of Code puzzle. Must be a number",
+    },
+    day: {
+      description:
+        "Day of the Advent Of Code puzzle. Must be a number (without a leading 0)",
+    },
+  })
+  .parse(process.argv);
+console.log(process.argv);
+console.log(values);
+
+// years[values.year][values.day]();
