f0766114d3b1e3349ace66db46e7f3938b75a4ac
Teddy Burns <code@teddyburns.com>
2024-12-07 10:09:23 -0500

    Day 1 Part 2

--- src/day01/day01.ts
+++ src/day01/day01.ts
@@ -33,14 +33,61 @@ const readInput: () => Promise<ListEntry[]> = async () => {
   return entries;
 };
 
-const main = async () => {
-  const entries = await readInput();
-  console.log("Entries:", entries);
-  let totalDistance = entries.reduce(
+const sortListEntries = (entries: ListEntry[]) => {
+  const left = new MaxHeap<number>();
+  const right = new MaxHeap<number>();
+  entries.forEach((entry) => {
+    left.push(entry.left);
+    right.push(entry.right);
+  });
+
+  const leftAsArray = left.sort();
+  const rightAsArray = right.sort();
+  const results: ListEntry[] = [];
+  for (let i = 0; i < left.size(); i++) {
+    results.push({ left: leftAsArray[i], right: rightAsArray[i] });
+  }
+
+  return results;
+};
+
+const calculateTotalDistance = (entries: ListEntry[]) => {
+  return entries.reduce(
     (acc, entry) => Math.abs(entry.left - entry.right) + acc,
     0
   );
+};
+
+const calculateSimilarityScore = (entries: ListEntry[]) => {
+  const leftItems = new Set(entries.map((entry) => entry.left));
+
+  type FrequencyMap = {
+    [key: number]: number;
+  };
+  const rightFrequencies = entries
+    .map((entry) => entry.right)
+    .filter((item) => leftItems.has(item))
+    .reduce((acc, number) => {
+      acc[number] = (acc[number] ?? 0) + 1;
+      return acc;
+    }, {} as FrequencyMap);
+
+  return Object.entries(rightFrequencies).reduce((acc, [number, frequency]) => {
+    const weightedOccurrence = parseInt(number) * frequency;
+    return acc + weightedOccurrence;
+  }, 0);
+};
+
+const main = async () => {
+  const entries = await readInput();
+  const sortedEntries = sortListEntries(entries);
+  console.log("Entries:", sortedEntries);
+
+  let totalDistance = calculateTotalDistance(sortedEntries);
   console.log("Total distance between list entries: %d", totalDistance);
+
+  let similarityScore = calculateSimilarityScore(sortedEntries);
+  console.log("Similarity score between lists: %d", similarityScore);
 };
 
 export default main;
